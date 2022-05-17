d1de453274280aa024a75fe9e0224ddfe7171bc8
druckmax <max_sommerfeld@web.de>
2022-05-17 15:24:51 +0200

    layout change

--- topics.js
+++ topics.js
@@ -2,6 +2,9 @@
 
 /* Example 1: Sorting the object keys in a descending order
 In this example we get the keys from 'obj' with the Object.keys method and then sort the keys starting from 20 down to .2. We change the deault ascending order by passing in a function that compares the values in the opposite order. This also helps us to compare numbers. Why? See below.*/
+console.log(`
+Example Sort:
+`);
 
 let obj = {
     '10': 1,
@@ -23,6 +26,10 @@ This is because of type coercion. Remember?
 // FILTER
 
 // Example: Get all the elements from the array which are a string
+console.log(`
+Example Filter:
+`);
+
 
 const arr = [1, 2, true, null, 'bananas', undefined, 'are', false, 'delicious']
 
@@ -34,6 +41,9 @@ console.log(filteredArr);
 //FOR EACH
 
 // Example 2: Back to our sorted object, or better said, sorted array of object keys. We now want to print each object key AND its corresponding value to the console. We retrieve the value from the object with obj[key] and its current 'key', but take over the order, which we created with sort in Example 1.
+console.log(`
+Example forEach:
+`);
 
 obj = {
     '10': 1,
@@ -46,6 +56,3 @@ obj = {
 sortedObj = Object.keys(obj)
                     .sort((a,b)=> b-a)
                     .forEach( key => console.log(`${obj[key]} x ${key}€`));
-
-
-console.log(sortedObj);
